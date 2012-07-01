f8c724ebc71cc05bf8ed771d961c235953a06b3a
nuriel <nuriel98@gmail.com>
2012-07-01 11:14:36 +0300

    with instagram, auth routing not yet working properly

--- app/controllers/screen_controller.rb
+++ app/controllers/screen_controller.rb
@@ -1,18 +1,18 @@
 class ScreenController < ApplicationController
   
   def index
-    # redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
+    redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
 
-    # client = Instagram.client(:access_token => session[:access_token])
-    #     @user = client.user
+    client = Instagram.client(:access_token => session[:access_token])
+    @user = client.user
     
     state = 1
     
     gon.state = state
     
-    
-      # gon.pics = client.tag_recent_media('mazeh9art')
-      gon.votes = Vote.all
+    gon.user = @user
+    gon.pics = client.tag_recent_media('mazeh9art')
+    gon.votes = Vote.all
     
   end
 

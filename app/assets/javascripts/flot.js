a4c76692badc26796459eef006869284dab21f23
nuriel <nuriel98@gmail.com>
2012-07-01 19:36:42 +0300

    styled flot, nexu is working!

--- app/assets/javascripts/flot.js
+++ app/assets/javascripts/flot.js
@@ -1,13 +1,26 @@
 $(document).ready( function () {
     if ($("#placeholder").length > 0) {
-			var d2 = [ [-1, 5], [0, 3], [1, 13] ];
+			var d0 = [ [-1, 5] ];
+			var d1 = [ [0, 3] ];
+			var d2 = [ [1, 13] ];
 			$.plot($("#placeholder"), [
-	        {
-	            data: d2,
-	            bars: { show: true, color: 'red' },
-					    color: 2,
+	        												{
+													            data: d0,
+													            bars: { show: true },
+																	    color: 2,
 							
-	        }
+													        },
+																	{
+													            data: d1,
+													            bars: { show: true },
+																	    color: 0,
+							
+													        },
+																	{
+													            data: d2,
+													            bars: { show: true },
+																	    color: 8
+													        }
 	    ]);
 	    
 		}
